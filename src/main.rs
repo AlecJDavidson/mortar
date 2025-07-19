3bef164da41ac139afdc05e19d5899e381b02e69
AlecJDavidson <alecjdavidson@outlook.com>
2025-07-19 16:18:17 -0400

    I can run test invocations. Time to connect a db.

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,31 @@ use axum::{
     routing::{get, post},
 };
 use std::net::SocketAddr;
+// use uuid::Uuid;
 
 mod handlers;
 mod structs;
-use crate::handlers::{bad_request, create_resource, hello_world, not_found_handler};
+use crate::handlers::{bad_request, create_resource, hello_world, invoke_brick, not_found_handler};
+// use crate::structs::{Brick, Language};
 
 #[tokio::main]
 async fn main() {
+    // Hardcoding a test brick
+    // let test_brick = Brick {
+    //     id: "n0nprod-00000001".to_string(),
+    //     name: "Test Brick".to_string(),
+    //     creation_time: "".to_string(),
+    //     last_invocation: "".to_string(),
+    //     language: Language::Bash,
+    //     source_path: "../test_scripts/hello.sh".to_string(), // Update this path to a valid Bash script for testing
+    //     active: true,
+    // };
+
     let app = Router::new()
         .route("/hello", get(hello_world)) // just a test endpoint so I don't go insane
         .route("/create", post(create_resource)) // unused for now
         .route("/bad-request", get(bad_request))
+        .route("/invoke-brick", post(invoke_brick)) // brick invocation
         .fallback(get(not_found_handler));
 
     // Run it on port 3000
--- src/structs.rs
+++ src/structs.rs
@@ -1,13 +1,31 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct ErrorResponse {
     pub status: String,
     pub message: String,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct SuccessResponse {
     pub status: String,
     pub message: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Brick {
+    pub id: String,
+    pub name: String,
+    pub creation_time: String, // DateTime<Utc>,
+    pub last_invocation: String, // Option<DateTime<Utc>>,
+    pub language: Language,
+    pub source_path: String,
+    pub active: bool
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Language {
+    Rust,
+    C,
+    Bash,
 }
